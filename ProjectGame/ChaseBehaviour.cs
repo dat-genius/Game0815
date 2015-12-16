aa66a495b65f64ee44be4302242c2bc31a2c62c6
Saliem <saliemamirkhan@hotmail.com>
2015-12-16 21:48:59 +0100

    ChaseBehaviour added (with own detection)

---
+++ ProjectGame/ChaseBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjectGame
+{
+    public class ChaseBehaviour : IBehaviour
+    {
+        public GameObject GameObject { get; set; }
+        private GameObject Player;
+        private float Radius;
+        private float lerpFactor = 0.01f;
+
+        public ChaseBehaviour(float radius, GameObject player)
+        {
+            Radius = radius;
+            Player = player;
+        }
+
+        public void OnUpdate(GameTime gameTime)
+        {
+            Vector2 beginPosition = GameObject.Position;
+            Vector2 positionDifference = beginPosition - (Player.Position);
+
+            if (positionDifference.X >= -Radius && positionDifference.X <= Radius || positionDifference.Y >= -Radius && positionDifference.Y <= Radius)
+            {
+                GameObject.Position = Vector2.Lerp(beginPosition, Player.Position, lerpFactor);
+            }
+        }
+
+        public void OnMessage(IMessage message)
+        {
+        }
+    }
+}
