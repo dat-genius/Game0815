01041b59d4c5083ded4aa6a50a2e466e726275c0
Dat-Genius <geniouserwin@gmail.com>
2016-01-07 13:48:40 +0100

    added a fix in chasebehaviour

--- TestProjectGame/FOVBehaviourTest.cs
+++ TestProjectGame/FOVBehaviourTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ProjectGame;
+using Microsoft.Xna.Framework;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestProjectGame
@@ -7,12 +8,31 @@ namespace TestProjectGame
     [TestClass]
     public class FOVBehaviourTest
     {
+        [TestInitialize]
+        
+
         [TestMethod]
-        public void SetViewDistanceTest()
+        public void TestHasFOVBehaviour()
+        {
+            GameObject testObject = new GameObject();
+            Assert.IsFalse(testObject.HasBehaviourOfType(typeof(FOVBehavior)));
+            testObject.AddBehaviour(new FOVBehavior());
+            Assert.IsTrue(testObject.HasBehaviourOfType(typeof(FOVBehavior)));
+        }
+
+        [TestMethod]
+        public void TestFind1()
         {
             GameObject testObject = new GameObject();
             testObject.AddBehaviour(new FOVBehavior());
-            Assert.IsTrue(testObject.HasBehaviourOfType(typeof(FOVBehavior)));
+            testObject.Rotation = 0;
+            testObject.Position = new Vector2(100, 100);
+            GameObject testPlayerObject = new GameObject();
+            testPlayerObject.AddBehaviour(new InputMovementBehaviour(5, new FollowCamera()));
+            testPlayerObject.Position = new Vector2(100, 200);
+            testObject.OnUpdate();
+            
+            
         }
     }
 }
