bdc6e5843ee2e37efd0f0dbb3ff4bb69f7090547
Dat-Genius <geniouserwin@gmail.com>
2016-01-07 12:34:39 +0100

    Added testproject
    added a testproject and the first test

---
+++ TestProjectGame/FOVBehaviourTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ProjectGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProjectGame
+{
+    [TestClass]
+    public class FOVBehaviourTest
+    {
+        [TestMethod]
+        public void SetViewDistanceTest()
+        {
+            GameObject testObject = new GameObject();
+            testObject.AddBehaviour(new FOVBehavior());
+            Assert.IsTrue(testObject.HasBehaviourOfType(typeof(FOVBehavior)));
+        }
+    }
+}
