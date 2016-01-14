9072e74de8be2a7dc12d8f751071de19ee43c0a2
Dat-Genius <geniouserwin@gmail.com>
2016-01-14 14:02:51 +0100

    Teleportbehaviour has been created
    the class teleportbehaviour is to be assigned to the gate of a castle,
    this will cause the player to be teleported to a different location

---
+++ ProjectGame/TeleportBlockBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ProjectGame
+{
+    public class TeleportBlockBehaviour : IBehaviour
+    {
+        public GameObject GameObject { get; set; }
+        public Vector2 TargetLocation { get; set; }
+
+        public TeleportBlockBehaviour(Vector2 target)
+        {
+            TargetLocation = target;
+        }
+
+        public void OnUpdate(GameTime gameTime)
+        {
+            
+        }
+
+        public void OnMessage(IMessage message)
+        {
+            if (message.GetType() == typeof(CollisionEnterMessage))
+            {
+                CollisionEnterMessage temp = (CollisionEnterMessage)message;
+                if (temp.CollidingObject.HasBehaviourOfType(typeof(InputMovementBehaviour)))
+                {
+                    temp.CollidingObject.Position = TargetLocation;
+                }
+            }
+        }
+    }
+}
