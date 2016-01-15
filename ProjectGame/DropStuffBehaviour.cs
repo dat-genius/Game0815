d3c30dde852ed4af55a8c47a9d284c23030f3ea6
Nerotruuv <mark.steijger@gmail.com>
2016-01-15 15:18:33 +0100

    dit zou de wereld moeten fixen

---
+++ ProjectGame/DropStuffBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectGame
+{
+    class DropStuffBehaviour: IBehaviour
+    {
+        GameObject Gameobject { get; set; }
+
+
+    }
+}
