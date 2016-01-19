691f57912396c9c8f9a901ed51840297950539f4
Dat-Genius <geniouserwin@gmail.com>
2016-01-19 11:01:30 +0100

    Finished FOVTests

--- TestProjectGame/FOVBehaviourTest.cs
+++ TestProjectGame/FOVBehaviourTest.cs
@@ -68,6 +68,7 @@ namespace TestProjectGame
         {
             GameObject testObject = new GameObject();
             FOVBehavior testFOV = new FOVBehavior(gameObjects);
+            testFOV.ViewDistance = 300;
             testObject.AddBehaviour(testFOV);
             testObject.Rotation = 0;
             testObject.Position = new Vector2(100, 100);
@@ -79,9 +80,97 @@ namespace TestProjectGame
             gameObjects.Add(testPlayerObject);
             testObject.CollidingGameObjects = gameObjects;
             testObject.OnUpdate(time);
-            FOVBehavior test = (FOVBehavior)testObject.GetBehaviourOfType(typeof(FOVBehavior));
-            Assert.IsTrue(test.DetectPlayer());
+            Assert.IsTrue(testFOV.DetectPlayer());
+        }
 
+        [TestMethod]
+        public void TestEdgeFindTrue()
+        {
+            GameObject testObject = new GameObject();
+            FOVBehavior testFOV = new FOVBehavior(gameObjects);
+            testObject.AddBehaviour(testFOV);
+            testObject.Rotation = 0;
+            testObject.Position = new Vector2(100, 100);
+            testFOV.ViewDistance = 100;
+            GameObject testPlayerObject = new GameObject();
+            testPlayerObject.AddBehaviour(new InputMovementBehaviour(5, new FollowCamera()));
+            testPlayerObject.Position = new Vector2(51, 50);
+            gameObjects.Add(testObject);
+            gameObjects.Add(testPlayerObject);
+            Assert.IsTrue(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(100, 1);
+            Assert.IsTrue(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(100, 99);
+            Assert.IsTrue(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(149, 50);
+            Assert.IsTrue(testFOV.DetectPlayer());
+        }
+
+        [TestMethod]
+        public void TestEdgeFindFalse()
+        {
+            GameObject testObject = new GameObject();
+            FOVBehavior testFOV = new FOVBehavior(gameObjects);
+            testObject.AddBehaviour(testFOV);
+            testObject.Rotation = 0;
+            testObject.Position = new Vector2(100, 100);
+            testFOV.ViewDistance = 100;
+            GameObject testPlayerObject = new GameObject();
+            testPlayerObject.AddBehaviour(new InputMovementBehaviour(5, new FollowCamera()));
+            testPlayerObject.Position = new Vector2(50, 50);
+            gameObjects.Add(testObject);
+            gameObjects.Add(testPlayerObject);
+            Assert.IsFalse(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(100, 100);
+            Assert.IsFalse(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(150, 50);
+            Assert.IsFalse(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(100, 0);
+        }
+
+        [TestMethod]
+        public void TestCornerFindTrue()
+        {
+            GameObject testObject = new GameObject();
+            FOVBehavior testFOV = new FOVBehavior(gameObjects);
+            testObject.AddBehaviour(testFOV);
+            testObject.Rotation = 0;
+            testObject.Position = new Vector2(100, 100);
+            testFOV.ViewDistance = 100;
+            GameObject testPlayerObject = new GameObject();
+            testPlayerObject.AddBehaviour(new InputMovementBehaviour(5, new FollowCamera()));
+            testPlayerObject.Position = new Vector2(51, 1);
+            gameObjects.Add(testObject);
+            gameObjects.Add(testPlayerObject);
+            Assert.IsTrue(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(51, 99);
+            Assert.IsTrue(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(149, 99);
+            Assert.IsTrue(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(149, 1);
+            Assert.IsTrue(testFOV.DetectPlayer());
+        }
+
+        [TestMethod]
+        public void TestCornerFindFalse()
+        {
+            GameObject testObject = new GameObject();
+            FOVBehavior testFOV = new FOVBehavior(gameObjects);
+            testObject.AddBehaviour(testFOV);
+            testObject.Rotation = 0;
+            testObject.Position = new Vector2(100, 100);
+            testFOV.ViewDistance = 100;
+            GameObject testPlayerObject = new GameObject();
+            testPlayerObject.AddBehaviour(new InputMovementBehaviour(5, new FollowCamera()));
+            testPlayerObject.Position = new Vector2(50, 0);
+            gameObjects.Add(testObject);
+            gameObjects.Add(testPlayerObject);
+            Assert.IsFalse(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(50, 100);
+            Assert.IsFalse(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(150, 100);
+            Assert.IsFalse(testFOV.DetectPlayer());
+            testPlayerObject.Position = new Vector2(150, 0);
         }
     }
 }
